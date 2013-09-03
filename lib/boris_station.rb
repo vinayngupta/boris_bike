690a55fe2911edf534cb8839d3740d9a48f86112
Vinay Gupta <vinaybeta@yahoo.com>
2013-09-03 18:05:48 +0100

    Day 2 of work on Boris Bike

--- lib/boris_station.rb
+++ lib/boris_station.rb
@@ -1,11 +1,29 @@
-
 class Station
-	attr_reader :name, :capacity
+	# attr_accessor :bikes, :broken
+	attr_reader :max_capacity, :bikes
 
-	def initialize(name, capacity)
-		@name 		= name
-		@capacity 	= capacity
-		
+	def initialize(bikes=[], max_capacity = 20)
+		@bikes = bikes
+		@max_capacity = max_capacity
 	end
 
+	def has_bikes?
+		!bikes.empty?
+	end
+
+	def release_bike
+		if !bikes.empty? && !has_broken_bikes?
+			return true
+		else
+			return false
+		end
+	end
+
+	def return_bike
+		bikes.length < max_capacity
+	end
+
+	def has_broken_bikes?
+		return true
+	end
 end
--- spec/boris_station_spec.rb
+++ spec/boris_station_spec.rb
@@ -1,17 +1,49 @@
 require 'boris_station'
 
 describe Station do
-	let (:station) { Station.new('Baker Street', 20) }
-	
-	it 'has a unique name' do
-		expect(station.name).to eq('Baker Street')
+	let(:station) {Station.new([:bike], 20)}
+
+	it 'does not have any bikes' do
+		#station = Station.new
+		#expect(station.has_bikes?).to be_true
+		expect(station).to have_bikes
 	end
 
-	it 'can hold up to 20 bikes' do
-		expect(station.capacity).to eq 20
+	it 'has bikes if passed in on creation' do
+		station = Station.new([:bike] ,8)
+		expect(station).to have_bikes
 	end
-	
-	it 'is empty when 0 bikes remain' do
-		expect(station.capacity).to eq 0
+
+	xit  'releases bikes if there are non-broken bikes available' do
+		expect(station.release_bike).to be_true
 	end
-end
+
+	it 'does not release bikes if there are no bikes' do
+		station = Station.new #empty station
+		expect(station.release_bike).to be_false
+	end
+
+	it 'does not release bikes if all the bikes are broken' do
+		station =Station.new #no non-broken bikes
+		expect(station).to have_broken_bikes
+
+	end
+
+	it 'does not accept bikes when it is full' do
+		full_station = Station.new([:bike] * 20)
+		expect(full_station.return_bike).to be_false
+	end
+
+	it 'does accept bikes when it is not full' do
+		full_station = Station.new([:bike] * 2) 
+		expect(full_station.return_bike).to be_true
+	end
+
+
+end	
+
+	# release thing if > 0
+	# accept things if not max capacity reached
+	# if broken thing cant release to user
+	# 	but can release to other thing (van)
+	# station has a max capacity
